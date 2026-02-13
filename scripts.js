12f2d461ecbb24ea7f7ba788e49ac97cb44dcdfd
Isaac Ziroldo <isaaclfr@gmail.com>
2026-02-13 11:22:44 -0300

    corrigido manipualação do input, para que seja possível informar números quebrados.

--- scripts.js
+++ scripts.js
@@ -7,16 +7,14 @@ const currency = document.getElementById("currency");
 
 //manipulando o input amount para receber somente números
 amount.addEventListener("input", ()=> {
-    const hasCharactersRegex = /\D+/g
-
+    const hasCharactersRegex = /[^\d.]/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
-
-// quotation.addEventListener("input", ()=> {
-//     const hasCharactersRegex = /\D+/g
-//     quotation.value = quotation.value.replace(hasCharactersRegex, "")
-// })
+quotation.addEventListener("input", ()=> {
+    const hasCharactersRegex = /[^\d.]/g
+    quotation.value = quotation.value.replace(hasCharactersRegex, "")
+})
 
 //Capturando o evento de submit do formulário
 form.onsubmit = (event) => {
@@ -37,5 +35,10 @@ form.onsubmit = (event) => {
 
 //Função para converter a moeda
 function convertCurrency(amount, price, symbol) {
-     console.log(amount, price, symbol)
+    try {
+        console.log(amount, price, symbol)
+    } catch (error) {
+        console.log(error);
+        alert("Ocorreu um erro ao converter a moeda. Verifique os valores inseridos e tente novamente.");
+    }
 }
